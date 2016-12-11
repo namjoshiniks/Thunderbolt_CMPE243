34e7286498ceeecb6b13bf8ad3ad3bd12c5ab983
Abhishek Singh <singh.abhishek21@gmail.com>
2016-12-11 07:05:36 -0800

    sensor_module: fixed sensor reading for 3 sensors

--- sensor_module/L5_Application/periodic_scheduler/period_callbacks.cpp
+++ sensor_module/L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -59,6 +59,7 @@ const uint32_t PERIOD_DISPATCHER_TASK_STACK_SIZE_BYTES = (512 * 3);
 /// Called once before the RTOS is started, this is a good place to initialize things once
 bool period_init(void)
 {
+	delay_ms(250);
 	//enable interrupt pins
 	eint3_enable_port2(0, eint_rising_edge, frontstartTimer);
 	eint3_enable_port2(0, eint_falling_edge, frontstopTimer);
@@ -142,32 +143,32 @@ void period_1Hz(uint32_t count)
 
 void period_10Hz(uint32_t count)
 {
-	static SENSOR_SONARS_t sonar_data;
-	updateCANsonar(&sonar_data);
-	can_msg_t can_msg = {0};
-
-	// Encode the CAN message's data bytes, get its header and set the CAN message's DLC and length
-
-	dbc_msg_hdr_t msg_hdr = dbc_encode_SENSOR_SONARS(can_msg.data.bytes, &sonar_data);
-	can_msg.msg_id = msg_hdr.mid;
-	can_msg.frame_fields.data_len = msg_hdr.dlc;
-
-	// Queue the CAN message to be sent out
-	if (Sensorcomplete)
-	{
-		if(CAN_tx(can1, &can_msg, 0))
-		{
-	//	   printf("Send data success\n");
-			Sensorcomplete = false;
-		}
-		else
-		{
-			printf("Send data fail!\n");
-		}
-
-	}
-
-	sendLEDmessage(leftDistance, frontDistance, rightDistance);
+//	static SENSOR_SONARS_t sonar_data;
+//	updateCANsonar(&sonar_data);
+//	can_msg_t can_msg = {0};
+//
+//	// Encode the CAN message's data bytes, get its header and set the CAN message's DLC and length
+//
+//	dbc_msg_hdr_t msg_hdr = dbc_encode_SENSOR_SONARS(can_msg.data.bytes, &sonar_data);
+//	can_msg.msg_id = msg_hdr.mid;
+//	can_msg.frame_fields.data_len = msg_hdr.dlc;
+//
+//	// Queue the CAN message to be sent out
+//	if (Sensorcomplete)
+//	{
+//		if(CAN_tx(can1, &can_msg, 0))
+//		{
+//	//	   printf("Send data success\n");
+//			Sensorcomplete = false;
+//		}
+//		else
+//		{
+//			printf("Send data fail!\n");
+//		}
+//
+//	}
+//
+//	sendLEDmessage(leftDistance, frontDistance, rightDistance);
 }
 
 void period_100Hz(uint32_t count)
@@ -180,29 +181,56 @@ void period_100Hz(uint32_t count)
 //		Sensor_RX2->setLow();
 //		vTaskDelay(10);
 		Sensor_RX1->setHigh();
-		delay_us(30);
+		delay_us(20);
 		Sensor_RX1->setLow();
 	}
-	else if(sensorCount == 4)
+	else if(sensorCount == 6)
 	{
 //		printf("DISTANCE 1 and 2 **************\n"); //testing
 //    	printf("Front: %i\n", frontDistance);
 //		printf("Back: %i\n", backDistance);
 		Sensor_RX2->setHigh();
-		delay_us(30);
+		delay_us(20);
 		Sensor_RX2->setLow();
 	}
-	else if(sensorCount == 8)
+	else if(sensorCount == 12)
 	{
 //        printf("DISTANCE 3 and 4 ===============\n"); //testing
 //        printf("Left: %i\n", leftDistance); //testing don't need delay
 //        printf("Right: %i\n", rightDistance); //testing don't need delay
 		  Sensor_RX3->setHigh();
-		  delay_us(30);
+		  delay_us(20);
 		  Sensor_RX3->setLow();
-		  Sensorcomplete = true;
-		sensorCount = -1;
+//		  Sensorcomplete = true;
+//		sensorCount = -1;
 	}
+	else if(sensorCount == 18)
+	{
+		static SENSOR_SONARS_t sonar_data;
+		updateCANsonar(&sonar_data);
+		can_msg_t can_msg = {0};
+
+		// Encode the CAN message's data bytes, get its header and set the CAN message's DLC and length
+
+		dbc_msg_hdr_t msg_hdr = dbc_encode_SENSOR_SONARS(can_msg.data.bytes, &sonar_data);
+		can_msg.msg_id = msg_hdr.mid;
+		can_msg.frame_fields.data_len = msg_hdr.dlc;
+
+		// Queue the CAN message to be sent out
+			if(CAN_tx(can1, &can_msg, 0))
+			{
+		//	   printf("Send data success\n");
+				Sensorcomplete = false;
+			}
+			else
+			{
+				printf("Send data fail!\n");
+			}
+
+		sendLEDmessage(leftDistance, frontDistance, rightDistance);
+		  sensorCount = -1;
+	}
+
 	sensorCount++;
 }
 
